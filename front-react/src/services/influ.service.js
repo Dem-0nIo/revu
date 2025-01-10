78cde56ca0e702626fabfc5dca5d76e5ea4d6118
David Millan <davide.millan@gmail.com>
2025-01-10 11:33:27 -0500

    :children_crossing: Gender field updated :children_crossing:

--- front-react/src/services/influ.service.js
+++ front-react/src/services/influ.service.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import authHeader from './auth-header';
 
 const API_URL = 'http://127.0.0.1:8081/api/influencer/';
+const GENDER_API_URL = 'http://127.0.0.1:8081/api/genders'; // Add Gender API endpoint
 
 const getAll = () => {
 	return axios.get(`${API_URL}all`, { headers: authHeader() });
@@ -19,11 +20,17 @@ const updateInfluencer = (values) => {
 	return axios.post(`${API_URL}update`, { values }, { headers: authHeader() });
 };
 
+// New method to fetch genders
+const getGenders = () => {
+	return axios.get(GENDER_API_URL); // No auth headers needed unless required
+};
+
 const UserService = {
 	getAll,
 	addInfluencer,
 	deleteInfluencer,
 	updateInfluencer,
+	getGenders, // Export the new method
 };
 
 export default UserService;
